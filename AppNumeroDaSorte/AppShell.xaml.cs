ea0015f2c527d8ee2fa0ba0a67d2e7f07bef72a1
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2025-11-05 13:37:45 -0400

    criacaoprojeto

---
+++ AppNumeroDaSorte/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppNumeroDaSorte
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppNumeroDaSorte/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppNumeroDaSorte")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppNumeroDaSorte.Pages")]
