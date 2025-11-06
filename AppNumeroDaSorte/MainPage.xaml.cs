20bc20a612af84cacabec4c5143d3584782d1ea7
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2025-11-06 10:31:07 -0400

    Numero da sorte

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -1,24 +1,34 @@
-﻿namespace AppNumeroDaSorte
+namespace AppNumeroDaSorte;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
-    {
-        int count = 0;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	private void OnGenerateLuckNumbers(object sender, EventArgs e)
+	{
+		NameApp.IsVisible = false;
+		ContainerLuckNumbers.IsVisible = true;
 
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object? sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+		var set = GenerateLuckNumbers();
+		luckNumber01.Text = set.ElementAt(0).ToString("D2");
+        luckNumber02.Text = set.ElementAt(1).ToString("D2");
+        luckNumber03.Text = set.ElementAt(2).ToString("D2");
+        luckNumber04.Text = set.ElementAt(3).ToString("D2");
+        luckNumber05.Text = set.ElementAt(4).ToString("D2");
+        luckNumber06.Text = set.ElementAt(5).ToString("D2");
     }
+
+	private SortedSet<int> GenerateLuckNumbers()
+	{
+		var set = new SortedSet<int>();
+			while (set.Count<6) {
+			var random = new Random();
+			var lucknumber = random.Next(1, 60);
+			set.Add(lucknumber);
+		}
+		return set;
+	}
+
 }
