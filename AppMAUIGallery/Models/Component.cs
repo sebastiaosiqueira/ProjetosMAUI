7ec9c4229deed9fe163c52df1afb0a16b0de5764
Sebastiao siqueira <sebastiao.valfrides@gmail.com>
2025-11-07 15:43:59 -0400

    Tabreconigze

---
+++ AppMAUIGallery/Models/Component.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Models
+{
+    internal class Component
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Type Page { get; set; }
+
+    }
+}
---
+++ AppTabbedPage/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace AppTabbedPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new TabbedPageMenu());
+        }
+    }
+}
